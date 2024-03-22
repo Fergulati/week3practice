78a35730f5899f0071825908dc89cc93bb1b2a08
Fergulati <demetrick@status.im>
2024-03-22 17:00:09 -0400

    removed the image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <div>
       <h1>Fergulati deployed to Vercel! Yay!</h1>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-      />
     </div>
   );
 }
